e9559176d4de07b1cda8f26f13e4d4a01792336c
aknielson <aknielson@hotmail.com>
2016-07-30 17:22:28 -0700

    Add project files.

---
+++ Console/DataSet1.cs
@@ -0,0 +1,15 @@
+﻿namespace Console
+{
+
+
+    partial class DataSet1
+    {
+    }
+}
+
+namespace Console.DataSet1TableAdapters {
+    
+    
+    public partial class DrillsTableAdapter {
+    }
+}
